55f61fae8856826ee40d8050de7c9473b7a27d16
David Ascienzo <dascienz@gmail.com>
2020-09-29 22:03:20 -0400

    Version 0.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as f:
 
 setup(
     name="tensorfit",
-    version="0.0.2",
+    version="0.0.4",
     description="Python package for univariate curve fitting using TensorFlow.",
     author="David Ascienzo",
     author_email="dascienz@gmail.com",
